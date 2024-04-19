eca14e83ecebcda18533b0220693c6fc8c56020c
Frico Simon Kefas <frico.kefas@dana.id>
2024-04-19 10:42:35 +0700

    Learn data structure in python

---
+++ dataStructure.py
@@ -0,0 +1,19 @@
+# like an array
+thisIsList = ["First", "second", "third"]
+
+print(thisIsList)
+
+# key value pair
+thisIsDictionary = {"name":"Friko", "age":22}
+
+print(thisIsDictionary["name"])
+
+# unique only
+thisIsSet = {1, 2, 1, 2, 3}
+
+print(thisIsSet)
+
+#tuple cant be changed later
+thisIsTuple = ("Dana Indonesia", "Capital Place", "Technical Support")
+
+print(thisIsTuple)
