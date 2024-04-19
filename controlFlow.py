7ad2b0a25598581cd0a15afc1a7332f27dd2167e
Frico Simon Kefas <frico.kefas@dana.id>
2024-04-19 10:42:18 +0700

    Learn control flow in python

---
+++ controlFlow.py
@@ -0,0 +1,19 @@
+# IF Else
+# renember indentation matter in python
+
+a = False
+if (a == True):
+    print("True")
+else:
+    print("False")
+
+# For loop 
+looping = [1,2,3,4,5]
+for item in looping:
+    print(item)
+
+# While loop
+num = 0 
+while (num < 5):
+    print(num)
+    num += 1
