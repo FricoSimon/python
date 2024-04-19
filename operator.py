e0dafb3879c6db8b27cef038a254ef7a121da08d
Frico Simon Kefas <frico.kefas@dana.id>
2024-04-19 10:40:57 +0700

    Learn operator in python

---
+++ operator.py
@@ -0,0 +1,31 @@
+# arithmethic operator
+
+print("Arithmethic")
+print(1+1)
+print(2-1)
+print(1*5)
+print(5**2)
+print(20 / 5)
+print(4%2)
+
+# string operator
+print("string")
+print("1"+"1")
+print("hello "*3)
+
+# comparison operator
+print("comparison")
+print(True==True)
+print(2 < 1)
+print(3 > 2)
+
+# logical operator
+print("logical")
+print(True and True)
+print(True or False)
+print(not True)
+
+# Membership
+print("membership")
+print(1 in (1,2,3))
+print(1 not in (2,3))
